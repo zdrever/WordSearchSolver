09d238a371d4ceec82c432ac2740e874023c3f53
conormcquitty <conormcquitty@hotmail.com>
2017-04-14 18:44:02 -0600

    Changed some lines to fit within vertical line on Atom.

--- UI.py
+++ UI.py
@@ -24,7 +24,8 @@ def resetprogram(root, frame):
     startscreen(root,frame)
 
 def startscreen(root, frame):
-    ''' Start screen shown at beginning of program. Contains Continue button which allows you to progress through the program.
+    ''' Start screen shown at beginning of program. Contains Continue button
+        which allows you to progress through the program.
 
     Args:
         root: window which contains all other widgets
@@ -33,13 +34,15 @@ def startscreen(root, frame):
     '''
 
     #prints entry message
-    entrymessage = Label(master = frame, text="Hello and welcome to Zach and Conor's Word Search Solver! \n Press the button below to continue!", font = (16))
+    entrymessage = Label(master = frame, text="Hello and welcome to Zach and\
+ Conor's Word Search Solver! \n Press the button below to continue!", font = (16))
     entrymessage.pack()
     entrymessage.place(relx=0.5, rely=0.5, anchor="center")
     root.update()
 
     #prints continue button, if clicked will go to choosefilescreen()
-    continue_button = Button(master = frame, text="Continue",font = (16),command = lambda : choosefilescreen(root, frame))
+    continue_button = Button(master = frame, text="Continue",font = (16),\
+    command = lambda : choosefilescreen(root, frame))
     continue_button.pack()
     continue_button.place(relx=0.5, rely=0.6, anchor = "center")
 
@@ -59,17 +62,21 @@ def choosefilescreen(root, frame):
     frame.pack()
 
     #displays prompt to choose file
-    secondarymessage = Label(master = frame, text="Please press the button below to choose your file. Choose the word array file first, and the word bank file second. ", font = 16)
+    secondarymessage = Label(master = frame, text="Please press the button\
+ below to choose your file. Choose the word array file first, and the word\
+ bank file second. ", font = 16)
     secondarymessage.pack()
     secondarymessage.place(relx=0.5, rely=0.5, anchor="center")
 
     #once 'choose file' button is pressed, go to openfile()
-    file_button = Button(master = frame, text = "Choose File", font = 16, command = lambda : openfile(root,frame))
+    file_button = Button(master = frame, text = "Choose File", font = 16, \
+    command = lambda : openfile(root,frame))
     file_button.pack()
     file_button.place(relx=0.5, rely=0.6, anchor="center")
 
 def openfile(root,frame):
-    ''' Opens window where user chooses file. Once files are chosen, continue to printing out the array and wordbank.
+    ''' Opens window where user chooses file. Once files are chosen, continue
+        to printing out the array and wordbank.
 
     Args:
         root: window which contains all other widgets
@@ -78,13 +85,17 @@ def openfile(root,frame):
     '''
 
     #Declaring which file types are allowed
-    allowedfiletypes = [ ('Allowed File Types', '*.txt ; *.png ; *.jpeg ; *.jpg'), ('All File Types', '*') ]
+    allowedfiletypes = [
+    ('Allowed File Types', '*.txt ; *.png ; *.jpeg ; *.jpg'), ('All File Types', '*')
+    ]
 
     #user picks the 2 files to use, first is array, second is word bank
-    #we use while loops to ensure the user has picked a file. If cancel is hit, the program will ask for the file again
+    #we use while loops to ensure the user has picked a file. If cancel is hit,
+    #the program will ask for the file again
     wordarrayfile = ''
     while wordarrayfile == '':
-        wordarrayfile = filedialog.askopenfilename(title = "Choose your array file", filetypes = allowedfiletypes)
+        wordarrayfile = filedialog.askopenfilename(title = "Choose your array file", \
+        filetypes = allowedfiletypes)
 
     wordbankfile = ''
     while wordbankfile == '':
@@ -122,7 +133,8 @@ def openfile(root,frame):
         dicttofind, wordcount = search.dictionaryfromfile('words.txt')
         foundwords = search.find_words(dicttofind, wordarray)
 
-    #checking if both files have been selected, once both files have been selected move to (printtextfiletoUI)
+    #checking if both files have been selected, once both files have been
+    #selected move to (printtextfiletoUI)
     a = 0
     if wordbankfile != '':
         a = 1
@@ -130,19 +142,24 @@ def openfile(root,frame):
         frame.destroy()
         frame = Frame(width=1500, height=950, bg="", colormap="new")
         frame.pack()
-        printtextfiletoUI(wordarray, heightofarray, widthofarray, root, frame, foundwords, dicttofind, wordcount)
+        printtextfiletoUI(wordarray, heightofarray, widthofarray, root, frame, \
+        foundwords, dicttofind, wordcount)
 
 
-def printtextfiletoUI(wordarray, heightofarray, widthofarray, root, frame, foundwords, dicttofind, wordcount):
-    ''' Prints the array file and word bank file to tkinter. Creates solve button that can be pressed to solve the wordsearch.
+def printtextfiletoUI(wordarray, heightofarray, widthofarray, root, frame, \
+                        foundwords, dicttofind, wordcount):
+    ''' Prints the array file and word bank file to tkinter. Creates solve button
+        that can be pressed to solve the wordsearch.
 
     Args:
-        wordarrary: instance of the CharArray class. It is an array created from a textfile.
+        wordarrary: instance of the CharArray class. It is an array created from
+                    a textfile.
         heightofarray: The height of the word array
         widthofarray: The width of the word array
         root: window which contains all other widgets
         frame: adjustable widget that other widgets can be placed on
-        foundwords: list of words found in the word array - only passed to be passed through to highlightsolution()
+        foundwords: list of words found in the word array - only passed to be
+                    passed through to highlightsolution()
         dicttofind: list of words to find, word bank created from this list
         wordcount: number of words to find. Used to create dimensions of word bank
 
@@ -201,9 +218,6 @@ def printtextfiletoUI(wordarray, heightofarray, widthofarray, root, frame, found
             for j in range(b):
                 textgrid.create_text(i*40 + 20, j*40 + 20, text = wordarray[j][i], font = 16)
 
-
-    #TODO: make wordbank fixed on the right side of the array rather than on one location
-
     #creating a wordbank the user can see, if less than 35 words we make one single list
     if c < 35:
 
@@ -231,7 +245,8 @@ def printtextfiletoUI(wordarray, heightofarray, widthofarray, root, frame, found
 
 
     #creating "solve" button, once hit continue to highlightsolution()
-    continuetosolver = Button(master = frame, text = "Solve", font = 16, command = lambda : highlightsolution(wordarray,foundwords, textgrid,root, frame))
+    continuetosolver = Button(master = frame, text = "Solve", font = 16, \
+    command = lambda : highlightsolution(wordarray,foundwords, textgrid,root, frame))
     continuetosolver.place(relx = 0.5, rely = 0.95, anchor = "center")
 
 
@@ -267,56 +282,72 @@ def highlightsolution(wordarray, FoundWord, textgrid,root, frame):
         if (dx, dy) == (-1,-1):
             for a in range(length):
                 #creates the line
-                textgrid.create_line(startingx + 25, startingy + 25, startingx - (length-1)*40 + 15, startingy - (length-1)*40 + 15, fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 25, startingy + 25, startingx - (length-1)*40 + 15,\
+                startingy - (length-1)*40 + 15, fill = "yellow", width = 4)
             for a in range(length):
-                #yellowraws the letters - I have to do two for loops or else it doesn't yellowraw properly
-                textgrid.create_text(startingx + 20 - a*40, startingy + 20 - a*40, text = wordarray[row-a][column-a], font=16)
+                #redraws the letters - I have to do two for loops or else it doesn't redraw properly
+                textgrid.create_text(startingx + 20 - a*40, startingy + 20 - a*40, \
+                text = wordarray[row-a][column-a], font=16)
 
         #vertical - sraight up
         if (dx, dy) == (-1,0):
             for a in range(length):
-                textgrid.create_line(startingx + 20, startingy + 32 , startingx + 20 , startingy - 40*a + 8 , fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 20, startingy + 32 , startingx + 20 ,\
+                startingy - 40*a + 8 , fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 , startingy - a*40 + 20, text = wordarray[row-a][column], font = 16)
+                textgrid.create_text(startingx + 20 , startingy - a*40 + 20, \
+                text = wordarray[row-a][column], font = 16)
 
         #diagonal - up and to the right
         if (dx, dy) == (-1,1):
             for a in range(length):
-                textgrid.create_line(startingx + 15, startingy + 25, startingx + length*40 - 15, startingy - (length - 1)*40 + 15, fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 15, startingy + 25, startingx + length*40 - 15, \
+                startingy - (length - 1)*40 + 15, fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 + a*40, startingy + 20 - a*40, text = wordarray[row - a][column + a], font = 16)
+                textgrid.create_text(startingx + 20 + a*40, startingy + 20 - a*40, \
+                text = wordarray[row - a][column + a], font = 16)
 
         #horizontal - to the left
         if (dx, dy) == (0,-1):
             for a in range(length):
-                textgrid.create_line(startingx + 32 , startingy + 20, startingx - 40*a + 8, startingy + 20, fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 32 , startingy + 20, startingx - 40*a + 8,\
+                startingy + 20, fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 - a*40 , startingy + 20, text = wordarray[row][column-a], font = 16)
+                textgrid.create_text(startingx + 20 - a*40 , startingy + 20,\
+                text = wordarray[row][column-a], font = 16)
 
         #horizontal - to the right
         if (dx, dy) == (0,1):
             for a in range(length):
-                textgrid.create_line(startingx + 8, startingy + 20, startingx + 40*length - 8, startingy + 20 , fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 8, startingy + 20, startingx + 40*length - 8, \
+                startingy + 20 , fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 + a*40 , startingy  + 20, text = wordarray[row][column+a], font = 16)
+                textgrid.create_text(startingx + 20 + a*40 , startingy  + 20,\
+                text = wordarray[row][column+a], font = 16)
 
         #diagonal - down and to the left
         if (dx, dy) == (1,-1):
             for a in range(length):
-                textgrid.create_line(startingx + 25, startingy + 15, startingx - (length-1)*40 + 15, startingy  + length*40 - 15, fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 25, startingy + 15, startingx - (length-1)*40 + 15, \
+                startingy  + length*40 - 15, fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 -a*40, startingy + 20 + a*40, text = wordarray[row + a][column - a], font = 16)
+                textgrid.create_text(startingx + 20 -a*40, startingy + 20 + a*40,\
+                 text = wordarray[row + a][column - a], font = 16)
 
         #vertical - straight down
         if (dx, dy) == (1,0):
             for a in range(length):
-                textgrid.create_line(startingx + 20, startingy + 8, startingx + 20, startingy + length*40 - 8 , fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 20, startingy + 8, startingx + 20,\
+                startingy + length*40 - 8 , fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 , startingy + a*40 + 20, text = wordarray[row+a][column], font = 16)
+                textgrid.create_text(startingx + 20 , startingy + a*40 + 20,\
+                text = wordarray[row+a][column], font = 16)
 
         #diagonal - down and to the right
         if (dx, dy) == (1,1):
             for a in range(length):
-                textgrid.create_line(startingx + 15, startingy + 15, startingx + length*40 - 15, startingy + length*40 - 15, fill = "yellow", width = 4)
+                textgrid.create_line(startingx + 15, startingy + 15, startingx + length*40 - 15, \
+                startingy + length*40 - 15, fill = "yellow", width = 4)
             for a in range(length):
-                textgrid.create_text(startingx + 20 + a*40, startingy + 20 + a*40, text=wordarray[row+a][column+a], font = 16)
+                textgrid.create_text(startingx + 20 + a*40, startingy + 20 + a*40, \
+                text=wordarray[row+a][column+a], font = 16)
